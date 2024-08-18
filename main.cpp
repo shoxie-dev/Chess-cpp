e46fbab07dca352031b71ac6cbdeeaf4933133f4
Joseph Williams <josephfranklinwilliams@gmail.com>
2024-08-18 11:28:09 +0200

    added comment

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 
 int main(){
     /*
-    TODO: basically just copy paste the rest of the shit ig for all the other pieces.
-    so next up on the agenda legal moves :)
-    
+    TODO: getting movement to work
+    then afterwards legal moves.
     make a pure virtual function in the base class that doesnt need to be defined there
     so it will be a boolean saying virtual bool isValidmove() = 0;
     */
